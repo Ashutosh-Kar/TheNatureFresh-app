8c46977f233f92cb8cd1417ad6de17130ff5c787
SAMBIT MAJHI <majhisambit2@gmail.com>
2020-08-04 01:27:58 +0530

    button removed and paddingadded

--- lib/pages/orderhsitory.dart
+++ lib/pages/orderhsitory.dart
@@ -55,6 +55,7 @@ class OrderCard extends StatelessWidget {
                   'Order Id: $id',
                   style: TextStyle(fontSize: 20),
                 ),
+                SizedBox(height: 8),
                 Text(
                   'ITEMS',
                   style: TextStyle(fontSize: 17),
@@ -62,7 +63,8 @@ class OrderCard extends StatelessWidget {
                 Text(
                   '1 x abcd, 2 x efgh',
                   style: TextStyle(fontSize: 14, color: Colors.grey.shade400),
-                ),
+                ),                
+                SizedBox(height: 4),
                 Text(
                   'ORDERED ON',
                   style: TextStyle(fontSize: 17),
@@ -77,43 +79,36 @@ class OrderCard extends StatelessWidget {
           Padding(
             padding: const EdgeInsets.all(8.0),
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              mainAxisAlignment: MainAxisAlignment.end,
               mainAxisSize: MainAxisSize.min,
               children: <Widget>[
-                MaterialButton(
-                  // padding: EdgeInsets.symmetric(horizontal:15, vertical:12),
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(15)),
-                  color: Colors.red,
-                  onPressed: () {
-                    print(
-                        "do backedn to repeat order adn transfer to chkout page");
-                    //OR, can navigate to login page and ask for login
-                    // credentials and then allow user to enter.
-                  },
-                  child: Text(
-                    'Repeat',
-                    style: TextStyle(color: Colors.white, fontSize: 18),
-                  ),
+                // MaterialButton(
+                //   // padding: EdgeInsets.symmetric(horizontal:15, vertical:12),
+                //   shape: RoundedRectangleBorder(
+                //       borderRadius: BorderRadius.circular(15)),
+                //   color: Colors.red,
+                //   onPressed: () {
+                //     print(
+                //         "do backedn to repeat order adn transfer to chkout page");
+                //     //OR, can navigate to login page and ask for login
+                //     // credentials and then allow user to enter.
+                //   },
+                //   child: Text(
+                //     'Repeat',
+                //     style: TextStyle(color: Colors.white, fontSize: 18),
+                //   ),
+                // ),
+                Text(
+                  'COD',
+                  style: TextStyle(fontSize: 16, color: Colors.grey.shade400),
                 ),
-                Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: <Widget>[
-                    Text(
-                      'COD',
-                      style:
-                          TextStyle(fontSize: 16, color: Colors.grey.shade400),
-                    ),
-                    Text(
-                      'Deliverd ',
-                      style: TextStyle(fontSize: 16),
-                    ),
-                    Text(
-                      'RS 100',
-                      style:
-                          TextStyle(fontSize: 17, fontWeight: FontWeight.bold),
-                    ),
-                  ],
+                Text(
+                  'Deliverd ',
+                  style: TextStyle(fontSize: 16),
+                ),
+                Text(
+                  'RS 100',
+                  style: TextStyle(fontSize: 17, fontWeight: FontWeight.bold),
                 ),
               ],
             ),
